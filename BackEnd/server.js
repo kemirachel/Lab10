4542aaa20c3c873a48ee3a4898616099e9c4c0b6
Kemi <G00379841@atu.ie>
2023-12-04 15:07:44 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 4000
-const cors = require('cors');
+// const cors = require('cors');
 
-{/*Made a new folder in React App and called BackEnd and added a file to that folder called
-server.js. */}
-{/*In server.js i intalled express and cors.*/}
-{/*In server.js in the terminal section i intalled body parser.*/}
+// {/*Made a new folder in React App and called BackEnd and added a file to that folder called
+// server.js. */}
+// {/*In server.js i intalled express and cors.*/}
+// {/*In server.js in the terminal section i intalled body parser.*/}
 
 
 
-{/*in my server.js i added a few lines of code to to avoid a CORS error.*/}
-app.use(cors());
+// {/*in my server.js i added a few lines of code to to avoid a CORS error.*/}
+// app.use(cors());
 app.use(function(req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -45,6 +45,20 @@ const bookSchema = new mongoose.Schema({
 })
 
 const bookModel = mongoose.model('books', bookSchema);
+
+
+
+// Serve the static files from the React app
+{/*Serve static files from the 'build' directory at the root level*/}
+{/*Serve static files from the 'build/static' directory under the '/static' route*/}
+
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
+
+
+
+
+
 
 app.delete('/api/book/:id', async (req,res)=>{
   console.log("Delete: "+req.params.id);
@@ -117,6 +131,15 @@ app.get('/api/book/:id', async (req,res)=>{
 })
 
 
+
+
+{/*add at the bottom just over app.listen*/}
+{/*Handles requests that don't match the ones above */}
+{/*Send the 'index.html' file as the response for any route*/}
+{/*dirname represents the current directory, and '/../build/index.html' is the path to the HTML file*/}
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/../build/index.html'));
+  });
 
 
 
